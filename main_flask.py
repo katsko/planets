10964565fab148a4c612d53f063bbb8c621978ac
Alexandr Katsko <alexandr.katsko@gmail.com>
2021-12-31 03:38:36 +0700

    cors

--- main_flask.py
+++ main_flask.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from flask import Flask
+from flask_cors import CORS
 from apium.handlers.flask.handler import api_handler
 
 
 app = Flask(__name__)
+CORS(app)
 app.add_url_rule('/api/', view_func=api_handler, methods=['POST'])
 app.run('localhost', 10800, debug=True)
