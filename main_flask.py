44f1657e14eb63177ab55893989b9ab728e1db5e
Alexandr Katsko <alexandr.katsko@gmail.com>
2021-12-31 03:55:30 +0700

    run server on 0.0.0.0:10800

--- main_flask.py
+++ main_flask.py
@@ -7,4 +7,4 @@ from apium.handlers.flask.handler import api_handler
 app = Flask(__name__)
 CORS(app)
 app.add_url_rule('/api/', view_func=api_handler, methods=['POST'])
-app.run('localhost', 10800, debug=True)
+app.run('0.0.0.0', 10800, debug=True)
